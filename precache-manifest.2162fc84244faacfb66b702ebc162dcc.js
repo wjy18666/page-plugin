c19163445b5e32ef66ea70305d79b443347bc241
王剑勇 <jianyong.wang@extreme-vision.com.cn>
2021-01-30 11:05:27 +0800

    init

---
+++ precache-manifest.2162fc84244faacfb66b702ebc162dcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4f8d7da48c3946fea7be7954e948fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceab6808eeaf152f17b3",
+    "url": "/static/css/2.192da617.chunk.css"
+  },
+  {
+    "revision": "66da65776c9657025494",
+    "url": "/static/css/main.0c8d8779.chunk.css"
+  },
+  {
+    "revision": "ceab6808eeaf152f17b3",
+    "url": "/static/js/2.17222c3c.chunk.js"
+  },
+  {
+    "revision": "66da65776c9657025494",
+    "url": "/static/js/main.0ba4003e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
